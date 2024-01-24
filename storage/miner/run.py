0499887e4d28d49f6cc669f3bdc71f645d26d805
eduardo <garciaruiz.edu@gmail.com>
2024-01-24 16:22:10 +0100

    fix flake errors: removing imported code but unused in ./storage/miner/run.py

--- storage/miner/run.py
+++ storage/miner/run.py
@@ -15,14 +15,14 @@
 # THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
-import time
-import wandb
+
 import bittensor as bt
-import traceback
+
 from substrateinterface import SubstrateInterface
 from scalecodec import ScaleBytes
+
 from .utils import update_storage_stats
-from copy import deepcopy
+
 
 tagged_tx_queue_registry = {
     "types": {
