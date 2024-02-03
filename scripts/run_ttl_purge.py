fa3c9a64ec2c54f02240399b977f64b25d9c25bf
ifrit98 <ifrit98@gmail.com>
2024-02-03 07:36:00 +0000

    add redis creds to ttl script

--- scripts/run_ttl_purge.py
+++ scripts/run_ttl_purge.py
@@ -5,12 +5,21 @@ import argparse
 import bittensor as bt
 from redis import asyncio as aioredis
 
+from storage.shared.utils import get_redis_password
 from storage.miner.database import purge_expired_ttl_keys
 
 
 async def main(args):
     try:
-        database = aioredis.StrictRedis(db=args.database_index)
+        redis_password = get_redis_password(args.redis_password)
+        database = aioredis.StrictRedis(
+            host=args.database_host,
+            port=args.database_port,
+            db=args.database_index,
+            socket_keepalive=True,
+            socket_connect_timeout=300,
+            password=redis_password,
+        )
 
         bt.logging.info(
             f"Purging expired TTL keys from database {args.database_index}..."
@@ -26,6 +35,9 @@ if __name__ == "__main__":
         parser = argparse.ArgumentParser()
         parser.add_argument("--netuid", type=int, default=21)
         parser.add_argument("--database_index", type=int, default=0)
+        parser.add_argument("--database_host", type=str, default="localhost")
+        parser.add_argument("--database_port", type=int, default=6379)
+        parser.add_argument("--redis_password", type=str, default=None)
         args = parser.parse_args()
 
         asyncio.run(main(args))
