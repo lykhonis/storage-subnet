f5ee21dd2ef5ec31ff5f62efcb8dc6701b5d9b18
eduardo <garciaruiz.edu+github@gmail.com>
2024-02-09 21:11:01 +0100

    using redis host to check_environment

--- neurons/validator.py
+++ neurons/validator.py
@@ -97,7 +97,7 @@ class neuron:
         print(self.config)
 
         try:
-            asyncio.run(check_environment(self.config.database.redis_conf_path))
+            asyncio.run(check_environment(self.config.database.redis_conf_path, self.config.database.host))
         except AssertionError as e:
             bt.logging.warning(
                 f"Something is missing in your environment: {e}. Please check your configuration, use the README for help, and try again."
