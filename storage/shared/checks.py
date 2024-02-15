9e529b8b246e93ba627bf474448f56ee8613ba5f
ifrit98 <ifrit98@gmail.com>
2024-02-15 23:43:12 +0000

    cleanup testkey after checks

--- storage/shared/checks.py
+++ storage/shared/checks.py
@@ -67,6 +67,9 @@ async def _check_data_persistence(redis_conf_path, port):
     # Retrieve data from Redis
     value = await new_redis.get("testkey")
 
+    # Clean up
+    await new_reids.delete("testkey")
+
     await new_redis.aclose()
     del new_redis
 
