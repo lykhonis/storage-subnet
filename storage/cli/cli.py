8b17342d1d5dec2938c4fea204ef03b88a71563a
eduardo <garciaruiz.edu+github@gmail.com>
2024-02-19 01:33:28 +0100

    recovering lost code from branch 'eduardogr/package/improve-packaging'

--- storage/cli/cli.py
+++ storage/cli/cli.py
@@ -27,6 +27,7 @@ from .retrievecommand import RetrieveData
 from .storecommand import StoreData
 from .listcommand import ListLocalHashes
 from .statscommand import ListMinerStats
+from .neuroncommand import RunMiner, RunValidator, RunApi
 
 # Create a console instance for CLI display.
 console = bittensor.__console__
@@ -69,6 +70,16 @@ COMMANDS = {
         "help": "Commands for retrieving statistics from the Bittensor network.",
         "commands": {
             "stats": ListMinerStats,  # lists all miner stats associated with hotkey
+        },
+    },
+    "run": {
+        "name": "run",
+        "aliases": ["n", "run"],
+        "help": "Commands for running neurons in this subnetwork.",
+        "commands": {
+            "miner": RunMiner,
+            "validator": RunValidator,
+            "api": RunApi,
         },
     },
 }
